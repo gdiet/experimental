e0d298dc95e7711650ccd7ce506a2dd5213a82b5
georg <gdiet@users.noreply.github.com>
2023-03-03 21:15:58 +0100

    research

--- src/main/scala/tryout/main.scala
+++ src/main/scala/tryout/main.scala
@@ -8,22 +8,28 @@ import scala.util.chaining.scalaUtilChainingOps
 
 @main def main(): Unit =
 
-  val b: Byte = 255.toByte
-  println(b)
-  println(2550 - b)
-
-  sys.exit(0)
+//  val b: Byte = 255.toByte
+//  println(b)
+//  println(2550 - b)
+//
+//  sys.exit(0)
 
   val random = SecureRandom()
   val scheme = com.codahale.shamir.Scheme(random, 15, 5)
   for (n <- 0 to 100) {
     print(s"$n ")
-    val secret = new Array[Byte](16)
+    val secret = new Array[Byte](16).tap(random.nextBytes)
     val parts = shamir.split(random, B(secret), 15, 5)
     val partsxy = parts.zipWithIndex.map((bytes, index) => Integer.valueOf(index + 1) -> bytes.bytes).toMap
     val recoveredx = scheme.join(partsxy.take(5).asJava)
     require(recoveredx.toSeq == secret.toSeq)
+    sys.exit(0)
   }
+
+def join(parts: Seq[Array[B]]): Array[B] =
+  parts.head.indices.map { i =>
+    B.zero
+  }.toArray
 
 def split(random: SecureRandom, secret: Array[B], shares: Int, threshold: Int): Seq[Array[B]] =
   require((shares < 256) && (shares > 0) && (threshold <= shares) && (threshold > 0)) // FIXME threshold 1 ???
