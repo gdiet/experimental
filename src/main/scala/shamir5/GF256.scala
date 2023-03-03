56eb5206c8a74f4e333b9da4b028fb7911c67df9
georg <gdiet@users.noreply.github.com>
2023-03-03 16:32:04 +0100

    research

--- src/main/scala/shamir5/GF256.scala
+++ src/main/scala/shamir5/GF256.scala
@@ -1,10 +1,15 @@
 package shamir5
 
+extension[T] (a: Array[T])
+  def apply(b: Byte): T = a(b.int)
+
 extension (b: Byte)
   def int: Int = java.lang.Byte.toUnsignedInt(b)
   def #+(c: Byte): Byte = (b ^ c).toByte
   def #-(c: Byte): Byte = (b ^ c).toByte
   def times3: Byte = if b >= 0 then (b << 1 ^ b).toByte else (b << 1 ^ b ^ 0x1b).toByte
+  def #*(c: Byte): Byte = if b == 0 || c == 0 then 0 else EXP(LOG(b) + LOG(c))
+  def #/(c: Byte): Byte = b #* EXP(255 - LOG(c))
 
 val EXP = { var a = 0xf6.toByte; Array.fill(510) { a = a.times3; a } }
 // TODO is LOG(0) used at all??? If not, it might be cleanest to set it to NAN.
