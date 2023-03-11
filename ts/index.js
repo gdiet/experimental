bba30d8151e5c814193c12ee0c1fb7e1d218cfa8
georg <gdiet@users.noreply.github.com>
2023-03-11 18:36:28 +0100

    log shares

--- ts/index.js
+++ ts/index.js
@@ -1,4 +1,31 @@
 "use strict";
+let math = {
+    mod: (a, n) => ((a % n) + n) % n,
+    inverse: (a, n) => {
+        for (let k = 1; k < n; k++)
+            if (math.mod(k * a, n) == 1)
+                return k;
+        throw new Error(`${a} is not invertible mod ${n}`);
+    },
+    exp: (a, e, n) => {
+        if (a == 0)
+            return 0;
+        let factor = e >= 0 ? a : math.inverse(a, n);
+        let result = 1;
+        for (let k = 1; k <= Math.abs(e); k++)
+            result = math.mod(result * factor, n);
+        return result;
+    },
+    calculatePolynomial: (x, coefficients, n) => foldLeft(coefficients, 0, (r, c, i) => math.mod(r + c * math.exp(x, i, n), n)),
+};
+/*
+const interpolate = (data, x, n) =>
+    foldLeft(data, 0, (r, e1, i) =>
+        mod(foldLeft(data, e1.y, (t, e2, j) =>
+            i == j ? t : mod(t * (x - e2.x) * inverse(e1.x - e2.x, n), n)
+        ) + r, 257)
+    )
+*/
 // document utilitiy functions
 let input = (id) => document.getElementById(id);
 let button = (id) => document.getElementById(id);
@@ -59,6 +86,16 @@ let aut = {
     displayPolynomial: () => span('polynomialSpan').innerHTML =
         foldLeft(cont.cleanedCoefficients, "P(x) = secret", (c, result, index) => `${result} + ${c}x<sup>${index + 1}</sup>`),
 };
+const createShares = () => {
+    let coefficients = cont.cleanedCoefficients;
+    let polynomial = foldLeft(coefficients, "P(x) = secret", (_, result, index) => `${result} + c${index + 1}*x^${index + 1}`);
+    for (let x = 1; x <= cont.numberOfShares; x++) {
+        let share = cont.secretNumbers.map(secret => math.calculatePolynomial(x, [secret].concat(coefficients), 257));
+        console.log(polynomial);
+        console.log(`P(${x})=${share.join(',')}`);
+    }
+};
+// wire the document functions
 aut.fillSecretNumbersFromText();
 aut.generateCoefficients();
 aut.displayPolynomial();
@@ -74,97 +111,5 @@ radio('staticCoefficientsRadio').addEventListener('change', aut.generateCoeffici
 radio('staticCoefficientsRadio').addEventListener('change', aut.displayPolynomial);
 input('staticCoefficientsInput').addEventListener('change', aut.fixStaticCoefficients);
 input('staticCoefficientsInput').addEventListener('change', aut.displayPolynomial);
-// // Dynamic document content functions
-// const coefficients = () => {
-//   let coefficientsText = byId('staticCoefficients').checked ? byId('userCoefficients').value : byId('generatedCoefficients').innerHTML
-//   let coefficientNumbers = coefficientsText.split(',').map(c => Number(c) || 0)
-//   let numberOfCoefficients = byId('threshold').value - 1
-//   return Array.from({length: numberOfCoefficients}, (_, index) => {
-//       if (coefficientNumbers[index] == 0 && index == numberOfCoefficients - 1) return 1
-//       return coefficientNumbers[index] < 0 ? 0 : coefficientNumbers[index]
-//   })
-// }
-// const setPolynomialAndGeneratedCoefficientsHtml = () => {
-//   let threshold = byId('threshold').value
-//   byId('generatedCoefficients').innerHTML = generatedCoefficients.join(',')
-//   let html = `P(x) = secret`
-//   for (let k = 1; k < threshold; k++) html += ` + ${coefficients()[k-1]}x<sup>${k}</sup>`
-//   byId('polynomial').innerHTML = html + ' | (mod 257)'
-// }
-/*
-
-// Helper functions for improved readability
-const foldLeft = (array, zero, func) => array.reduce(func, zero)
-
-// Maths for secret sharing and reconstruction
-const mod = (a, n) => ((a % n) + n) % n
-const inverse = (a, n) => {
-    for (let k = 1; k < n; k++) if (mod(k * a, n) == 1) return k
-    throw new Error(`${a} is not invertible mod ${n}`)
-}
-const interpolate = (data, x, n) =>
-    foldLeft(data, 0, (r, e1, i) =>
-        mod(foldLeft(data, e1.y, (t, e2, j) =>
-            i == j ? t : mod(t * (x - e2.x) * inverse(e1.x - e2.x, n), n)
-        ) + r, 257)
-    )
-const exp = (a, e, n) => {
-    if (a == 0) return 0
-    let factor = e >= 0 ? a : inverse(a, n)
-    let result = 1
-    for (let k = 1; k <= Math.abs(e); k++) result = mod(result * factor, n)
-    return result
-}
-const calculatePolynomial = (x, coefficients, n) =>
-  foldLeft(coefficients, 0, (r, c, i) => mod(r + c * exp(x, i, n), n))
-
-
-  
-// min, max: The minimum is inclusive and the maximum is exclusive
-const random = (min, max) => Math.floor(Math.random() * (max - min) + min)
-
-
-
-listen('numberOfShares', 'change', () => {
-    let numberOfSharesRead = Math.floor(Math.abs(Number(byId('numberOfShares').value)))
-    byId('numberOfShares').value = Math.min(256, Math.max(byId('threshold').value, numberOfSharesRead))
-})
-listen('threshold', 'change', () => {
-    let thresholdRead = Math.floor(Math.abs(Number(byId('threshold').value)))
-    byId('threshold').value = Math.min(byId('numberOfShares').value, Math.max(2, thresholdRead))
-    setPolynomialAndGeneratedCoefficientsHtml()
-})
-
-listen('createSharesButton', 'click',  () => {
-    for (let x = 1; x <= byId('numberOfShares').value; x++) {
-        let secretNumbers = byId('secretNumbersInput').value.split(',')
-        console.log("share for x", x)
-        for (let k = 0; k < secretNumbers.length; k++) {
-            let polynomialCoefficients = [secretNumbers[k]].concat(coefficients())
-            let y = calculatePolynomial(x, polynomialCoefficients, 257)
-            console.log(x,secretNumbers[k],y)
-        }
-    }
-})
-
-// to create a share for x,
-// the first coefficient must be the secret,
-// the middle coefficients should be random numbers,
-// the last coefficient modulus 257 must not be 0.
-// the total number of coefficients (including share)
-// is the number of shares needed for reconstruction.
-// console.log(calculatePolynomial(2, [4,6,3], 257))
-
-
-// https://developer.mozilla.org/en-US/docs/Web/API/SubtleCrypto/generateKey
-// for the signature - consider using a ecc key pair instead of rsa
-
-// const toHex        = byte  => byte.toString(16).padStart(2, '0')
-// const toHexSpaced  = array => array.map(byte => toHex(byte)).join(" ")
-// const toHexCompact = array => array.map(byte => toHex(byte)).join("")
-
-// const randomByte   = ()         => random(0, 256)
-// const randomBytes  = count      => Array.from({length: count}, () => randomByte() )
-
-*/
+button('createSharesButton').addEventListener('click', createShares);
 //# sourceMappingURL=index.js.map
