23a107e737c8e31d2a7e3ef1f501be7e341d96f1
georg <gdiet@users.noreply.github.com>
2024-07-12 17:01:56 +0200

    add "Move from ant stack" test case

--- src/test/scala/ants/MaybeMoveSpec.scala
+++ src/test/scala/ants/MaybeMoveSpec.scala
@@ -6,7 +6,10 @@ import matchers._
 
 class MaybeMoveSpec extends AnyFlatSpec with should.Matchers:
   val terrain = Terrain(XY(10, 10))
-  val ant = Ant("1", Player("1"))
+  val ant = Ant("1", Player("A"))
+  val ant2 = Ant("2", Player("A"))
+  val ant3 = Ant("3", Player("A"))
+  val ant1b = Ant("1", Player("B"))
 
   "Not enough power: 'NN' from (6, 6) with power 1 and weight 2" should "result in None with power 1" in {
     val board = Board(terrain, Map(ant -> AntState(XY(6, 6), 1)))
@@ -47,6 +50,19 @@ class MaybeMoveSpec extends AnyFlatSpec with should.Matchers:
   "Downhill move: 'NNNN' from (6, 6) elevation 2 with power 1 and weight 1" should "result in (6, 4) with power 0" in {
     val terrain = Terrain(XY(10, 10), Map(XY(6, 6) -> Cell(2, 0)))
     val board = Board(terrain, Map(ant -> AntState(XY(6, 6), 1)))
+    val result = maybeMove(ant, "NNNN", 1, board)
+    result should be(Some(AntState(XY(6, 3), 0)))
+  }
+
+  "Move from ant stack: 'NNNN' from (6, 6) where 3 own and one foreign ants are with power 1 and weight 1" should "result in (6, 4) with power 0" in {
+    val terrain = Terrain(XY(10, 10))
+    val antState = AntState(XY(6, 6), 1)
+    val board = Board(terrain, Map(
+      ant -> antState,
+      ant2 -> antState,
+      ant3 -> antState,
+      ant1b -> antState
+    ))
     val result = maybeMove(ant, "NNNN", 1, board)
     result should be(Some(AntState(XY(6, 3), 0)))
   }
